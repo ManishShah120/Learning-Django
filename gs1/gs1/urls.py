971a410e070fde22445595a74d3406c9d3ccf21e
ManishShah120 <mkshah141@gmail.com>
2021-01-12 14:29:32 +0530

    GS1
    Create Function Based View

---
+++ gs1/gs1/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from course import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('learndj/',views.learn_django),
+    path('learnpy/',views.learn_python),
+]
