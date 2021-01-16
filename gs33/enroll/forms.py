66822e45e08b60ea87b272645d71a2548694a769
ManishShah120 <mkshah141@gmail.com>
2021-01-16 20:22:30 +0530

    GS33
    Loop Form Hidden Field

---
+++ gs33/enroll/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class StudentRegistration(forms.Form):
+    name = forms.CharField()
+    email = forms.EmailField()
+    mobile = forms.IntegerField()
+    key = forms.CharField(widget=forms.HiddenInput())
